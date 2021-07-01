557bdb896db7bd49ce934d33fa949d930f2863ef
Suning Yao <1651500561@qq.com>
2021-07-01 12:22:36 +0800

    update deploy to use https > ssh for git

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f https://github.com/fewwwww/suningyao.git master:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:fewwwww/suningyao.git master:gh-pages
+# git push -f git@github.com:fewwwww/suningyao.git master:gh-pages
 
 cd -
