4c3018a23245776d278408d161de72e2c31c8b71
Suning Yao <sy2940@nyu.edu>
2021-09-11 17:55:52 -0400

    change repo name for deploying

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f https://github.com/fewwwww/suningyao.git master:gh-pages
+git push -f https://github.com/fewwwww/suningyao.com.git master:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:fewwwww/suningyao.git master:gh-pages
